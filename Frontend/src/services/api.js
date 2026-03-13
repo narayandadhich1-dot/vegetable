d9f5df637211fe3cf673683d2ef0da0eb3942921
Narayan Dadhich <narayandadhich1@gmail.com>
2026-03-13 18:58:14 +0530

    add

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:8000/api/v1"
+    ? "http://localhost:8000/"
     : "https://vegetable-rk3y.onrender.com/api/v1";
 
 const API = axios.create({
